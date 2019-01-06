757d81b924701e07338894a7ecf9f0d1f229bd66
Tasnim <tasnim.tanveer@gmail.com>
2019-01-05 20:52:28 -0600

    Table Formatting Function and Test

--- issues/lib/cli.ex
+++ issues/lib/cli.ex
@@ -1,4 +1,5 @@
 defmodule Issues.CLI do
+  import Issues.TableFormatter, only: [print_table_for_columns: 2]
   @default_count 4
 
   @moduledoc """
@@ -57,6 +58,7 @@ defmodule Issues.CLI do
     |> decode_response()
     |> sort_into_descending_order()
     |> last(count)
+    |> print_table_for_columns(["number", "created_at", "title"])
   end
 
   def decode_response({:ok, body}), do: body
