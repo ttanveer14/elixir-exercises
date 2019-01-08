461eedac003640101c9aa5acbf9a14ab2cdb428e
Tasnim <tasnim.tanveer@gmail.com>
2019-01-07 23:09:29 -0600

    Basic Weather Exercise Solution

--- weather/test/getfile_test.exs
+++ weather/test/getfile_test.exs
@@ -2,11 +2,10 @@ defmodule GetfileTest do
   use ExUnit.Case
   #use ExUnit.CaptureIO
 
-  alias Weather.Getfile, as: GF
+  alias Weather.Getfile
 
   test "opens file successfully" do
-    body = GF.readxml("https://w1.weather.gov/xml/current_obs/KMDW.xml")
+    body = Getfile.readxml("https://w1.weather.gov/xml/current_obs/KMDW.xml")
     assert body != {}
-    IO.inspect(body)
   end
 end
