5decf748ed24b5ab116c37b4b60540ab6b478230
Tasnim <tasnim.tanveer@gmail.com>
2019-01-20 12:08:27 -0600

    Creation of Chain Example

---
+++ chain/lib/chain.ex
@@ -0,0 +1,18 @@
+defmodule Chain do
+  @moduledoc """
+  Documentation for Chain.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Chain.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
