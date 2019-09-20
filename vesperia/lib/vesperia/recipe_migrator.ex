4496406b802d7e8ffbb86e8fc2e8f77d828a491f
Tasnim <tasnim.tanveer@gmail.com>
2019-09-20 17:03:29 -0500

    parse recipes into map

---
+++ vesperia/lib/vesperia/recipe_migrator.ex
@@ -0,0 +1,79 @@
+defmodule Vesperia.RecipeMigrator do
+  @recipes_list "./priv/vesperia-recipes-effects.txt"
+
+  def parse_recipe_names do
+    @recipes_list
+    |> File.stream!()
+    |> Stream.map(&parse_recipe_name/1)
+    |> Stream.reject(&is_nil/1)
+    |> Enum.reject(fn line -> line in [:-, :""] end)
+    |> mapify_recipes([])
+  end
+
+  defp parse_recipe_name("///" <> _tail) do
+    nil
+  end
+
+  defp parse_recipe_name("_" <> _tail) do
+    nil
+  end
+
+  defp parse_recipe_name("  " <> tail) do
+    parse_ingredients(tail, String.contains?(tail, "--"))
+  end
+
+  defp parse_recipe_name(line) do
+    line
+    |> String.split("--", parts: 2, trim: true)
+    |> Enum.at(0)
+    |> String.trim_trailing()
+    |> String.downcase()
+    |> String.replace(" ", "_")
+    |> String.to_atom()
+  end
+
+  defp parse_ingredients(_, false), do: nil
+
+  defp parse_ingredients(line, true) do
+    line
+    |> String.split("--", parts: 2, trim: true)
+    |> Enum.at(0)
+    |> String.trim()
+    |> String.split(",", trim: true)
+    |> Stream.map(&String.trim/1)
+    |> Stream.map(&String.downcase/1)
+    |> Stream.map(&String.split(&1, "(", trim: true))
+    |> Enum.map(&ingredient_and_qty/1)
+  end
+
+  defp ingredient_and_qty([ingredient]) do
+    {atomify(ingredient), 1}
+  end
+
+  defp ingredient_and_qty([ingredient, qty]) do
+    {atomify(ingredient), get_number(qty)}
+  end
+
+  defp atomify(ingredient) do
+    ingredient
+    |> String.trim()
+    |> String.replace(" ", "_")
+    |> String.to_atom()
+  end
+
+  defp get_number(qty) do
+    qty
+    |> String.codepoints()
+    |> Stream.filter(fn char -> String.contains?("0123456789", char) end)
+    |> Enum.at(0)
+    |> String.to_integer()
+  end
+
+  defp mapify_recipes([], keyword_list), do: Map.new(keyword_list)
+
+  defp mapify_recipes([_dog_food], keyword_list), do: Map.new(keyword_list)
+
+  defp mapify_recipes([recipe_name, ingredient_list | rest], keyword_list) do
+    mapify_recipes(rest, [{recipe_name, Map.new(ingredient_list)} | keyword_list])
+  end
+end
--- vesperia/lib/vesperia/recipes.ex
+++ vesperia/lib/vesperia/recipes.ex
@@ -1,7 +1,70 @@
 defmodule Vesperia.Recipes do
   def recipes do
-    [
-      %{name: "Sandwich", ingredients: [:bread, :egg]}
-    ]
+    %{
+      okonomiyaki: %{
+        cabbage: 1,
+        egg: 1,
+        meat: 1,
+        shrimp: 1,
+        squid: 1,
+        sticky_flour: 1
+      },
+      minestrone_soup: %{onion: 1, tomato: 1},
+      pudding: %{egg: 1, milk: 1},
+      cream_stew: %{carrot: 1, chicken: 1, milk: 1, onion: 1, potato: 1},
+      beef_bowl: %{beef: 1, onion: 1, rice: 1},
+      omelette_rice: %{chicken: 1, egg: 1, onion: 1, rice: 1},
+      miso_soup: %{miso: 1, radish: 1, tofu: 1},
+      tempura: %{
+        chicken: 1,
+        egg: 1,
+        shrimp: 1,
+        squid: 1,
+        sticky_flour: 1,
+        vegetable: 1
+      },
+      rice_ball: %{dried_seaweed: 1, rice: 1, salmon: 1},
+      mabo_curry: %{onion: 1, potato: 1, rice: 1, tofu: 1, tomato: 1},
+      sashimi: %{mackerel: 1, salmon: 1, scallop: 1, squid: 1, tuna: 1},
+      fruit_parfait: %{egg: 1, fruit: 4, milk: 1},
+      pork_miso_soup: %{carrot: 1, miso: 1, pork: 1, tofu: 1},
+      meat_sauce: %{onion: 1, pork: 1, sticky_flour: 1, tomato: 1},
+      scottish_egg: %{egg: 1, meat: 1, tomato: 1},
+      vichyssoise: %{milk: 1, potato: 1, sticky_flour: 1},
+      salad: %{cucumber: 1, lettuce: 1, tomato: 1},
+      sandwich: %{bread: 1, egg: 1},
+      soup_noodles: %{cabbage: 1, carrot: 1, onion: 1, pork: 1, sticky_flour: 1},
+      salisbury_steak: %{beef: 1, egg: 1, onion: 1},
+      japanese_stew: %{kelp: 1, radish: 1, squid: 1},
+      fried_chicken_and_fries: %{chicken: 1, potato: 1},
+      clam_chowder: %{onion: 1, potato: 1, scallop: 1},
+      croquette: %{meat: 1, onion: 1, potato: 1},
+      cake: %{egg: 1, milk: 1, strawberry: 1},
+      vegetable_stiry_fry: %{carrot: 1, cucumber: 1, meat: 1, onion: 1},
+      seafood_bowl: %{
+        cucumber: 1,
+        rice: 1,
+        salmon: 1,
+        scallop: 1,
+        shrimp: 1,
+        tuna: 1
+      },
+      fish_with_miso_sauce: %{mackerel: 1, miso: 1, rice: 1},
+      crepe: %{banana: 1, egg: 1, kiwifruit: 1, milk: 1},
+      sushi: %{dried_seaweed: 1, fish: 3, kelp: 1, rice: 1},
+      udon_noodles_hot_pot: %{
+        egg: 1,
+        napa_cabbage: 1,
+        shiitake: 1,
+        shrimp: 1,
+        squid: 1,
+        sticky_flour: 1
+      },
+      sorbet: %{fruit: 1, milk: 1},
+      pork_stew: %{kelp: 1, pork: 1, radish: 1},
+      sukiyaki: %{beef: 2, egg: 1, napa_cabbage: 1, shiitake: 1, tofu: 1},
+      kebab_sandwich: %{beef: 1, bread: 1, lettuce: 1, tomato: 1},
+      curry: %{carrot: 1, meat: 1, onion: 1, potato: 1, rice: 1}
+    }
   end
 end
