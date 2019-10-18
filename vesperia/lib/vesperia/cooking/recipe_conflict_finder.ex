40542905f9e814d329265ecc56781119cef5b1c5
Tasnim <tasnim.tanveer@gmail.com>
2019-10-17 21:54:17 -0500

    refactor conflict finder

--- vesperia/lib/vesperia/cooking/recipe_conflict_finder.ex
+++ vesperia/lib/vesperia/cooking/recipe_conflict_finder.ex
@@ -1,58 +1,52 @@
 defmodule Vesperia.Cooking.RecipeConflictFinder do
-  @recipes Vesperia.Info.Recipes.recipes()
+  # @recipes Vesperia.Info.Recipes.recipes()
+  @ingredients_used_in_recipes Vesperia.Info.Recipes.ingredients_used_in_recipes()
   @ingredient_types Vesperia.Info.Ingredients.ingredient_types()
-  @ingredents_by_type Vesperia.Info.Ingredients.ingredient_groups()
+  @ingredients_by_type Vesperia.Info.Ingredients.ingredient_groups()
 
   def choose_recipes([_single_recipe]), do: %{hard_conflicts: %{}, soft_conflicts: []}
 
   def choose_recipes(recipe_names) when is_list(recipe_names) do
-    @recipes
-    |> Stream.filter(fn {recipe_name, _ingredients} -> recipe_name in recipe_names end)
-    |> Stream.map(fn {_recipe_name, ingredients} -> ingredients end)
-    |> Enum.reduce(%{}, fn ingredients, acc ->
-      Map.merge(acc, ingredients, fn _ingredient, qty_1, qty_2 -> qty_1 + qty_2 end)
+    @ingredients_used_in_recipes
+    |> Stream.filter(fn {_ingredient, recipes} ->
+      Enum.any?(recipe_names, &Map.has_key?(recipes, &1))
     end)
+    |> Stream.map(fn {ingredient, recipes} -> {ingredient, Map.take(recipes, recipe_names)} end)
     |> calculate_soft_conflicts()
     |> calculate_hard_conflicts()
-    |> format_conflicts()
+    |> Map.take([:soft_conflicts, :hard_conflicts])
   end
 
-  defp calculate_soft_conflicts(%{} = ingredients_and_quantities) do
-    recipe_ingredient_types =
-      ingredients_and_quantities
-      |> Enum.filter(fn {ingredient, _qty} -> ingredient in @ingredient_types end)
-
-    recipe_ingredient_conflicts =
-      Enum.map(recipe_ingredient_types, fn {type, _qty} ->
-        Enum.filter(ingredients_and_quantities, fn {ingredient, _qty} ->
-          ingredient in Map.fetch!(@ingredents_by_type, type)
+  defp calculate_soft_conflicts(ingredients_in_recipes) do
+    soft_conflicts =
+      ingredients_in_recipes
+      |> Stream.filter(fn {ingredient, _recipes} -> ingredient in @ingredient_types end)
+      |> Stream.map(fn {ingredient_type, type_recipes} ->
+        Enum.reduce(ingredients_in_recipes, %{ingredient_type => type_recipes}, fn
+          {ingredient, recipes}, acc ->
+            if ingredient in Map.fetch!(@ingredients_by_type, ingredient_type) do
+              Map.put(acc, ingredient, recipes)
+            else
+              acc
+            end
         end)
       end)
+      |> Enum.reject(fn type -> map_size(type) === 1 end)
 
-    Map.put(
-      ingredients_and_quantities,
-      :soft_conflicts,
-      recipe_ingredient_types
-      |> Stream.zip(recipe_ingredient_conflicts)
-      |> Enum.map(fn {type, list} -> Map.new([type | list]) end)
-    )
+    %{
+      soft_conflicts: soft_conflicts,
+      ingredients_in_recipes: ingredients_in_recipes
+    }
   end
 
-  defp calculate_hard_conflicts(%{} = ingredients_and_conflicts) do
+  defp calculate_hard_conflicts(ingredients_with_soft_conflicts)
+       when is_map(ingredients_with_soft_conflicts) do
     hard_conflicts =
-      ingredients_and_conflicts
-      |> Map.drop([:soft_conflicts])
-      |> Stream.map(fn
-        {ingredient, qty} when qty > 1 -> {ingredient, qty}
-        {_ingredient, _one} -> nil
-      end)
-      |> Enum.reject(&is_nil/1)
-      |> Map.new()
+      ingredients_with_soft_conflicts
+      |> Map.fetch!(:ingredients_in_recipes)
+      |> Stream.reject(fn {_ingredient, recipes} -> map_size(recipes) === 1 end)
+      |> Enum.into(%{})
 
-    Map.put(ingredients_and_conflicts, :hard_conflicts, hard_conflicts)
-  end
-
-  defp format_conflicts(%{hard_conflicts: hard_conflicts, soft_conflicts: soft_conflicts}) do
-    %{hard_conflicts: hard_conflicts, soft_conflicts: soft_conflicts}
+    Map.put(ingredients_with_soft_conflicts, :hard_conflicts, hard_conflicts)
   end
 end
