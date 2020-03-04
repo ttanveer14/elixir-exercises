17f48986df0bcc7ece281e49b4146b366adbbd9d
Tasnim <tasnim.tanveer@gmail.com>
2020-03-04 09:32:04 -0600

    creating somewhat readable page in live view

--- vesperia/lib/vesperia_web/live/test.ex
+++ vesperia/lib/vesperia_web/live/test.ex
@@ -1,13 +1,19 @@
 defmodule VesperiaWeb.Live.Test do
   use Phoenix.LiveView
 
+  alias Vesperia.Cooking.RecipeConflictFinder
+
   def render(assigns) do
-    ~L"""
-    Hello World!
-    """
+    Phoenix.View.render(VesperiaWeb.RecipeConflictView, "live.html", assigns)
   end
 
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, assign(socket, :recipe_conflicts, %{soft_conflicts: [], hard_conflicts: %{}})}
+  end
+
+  def handle_event("calculate_conflicts", %{"query" => query}, socket) do
+    {:ok, recipe_conflicts} = RecipeConflictFinder.find_conflicts(query)
+
+    {:noreply, assign(socket, :recipe_conflicts, recipe_conflicts)}
   end
 end
