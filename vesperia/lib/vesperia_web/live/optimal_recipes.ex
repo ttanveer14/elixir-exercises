f9a44e69e9950a5e35c4f3a678378bd58a2f79eb
Tasnim <tasnim.tanveer@gmail.com>
2020-03-06 18:52:52 -0600

    found that there must be an issue with underlying liveview

--- vesperia/lib/vesperia_web/live/optimal_recipes.ex
+++ vesperia/lib/vesperia_web/live/optimal_recipes.ex
@@ -4,18 +4,12 @@ defmodule VesperiaWeb.Live.OptimalRecipes do
   alias Vesperia.Cooking.RecipeOptimizer
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, optimal_recipe_combos: [], requests: 0)}
+    {:ok, assign(socket, optimal_recipe_combos: [])}
   end
 
   def render(assigns) do
     Phoenix.View.render(VesperiaWeb.OptimalRecipesView, "index.html", assigns)
   end
-
-  # def handle_event("find_optimal_recipes", _params, %{assigns: %{requests: requests}} = socket)
-  #     when requests > 0 do
-  #   IO.inspect("SUCCESSFULLY RAN INTO THE WALL")
-  #   {:noreply, socket}
-  # end
 
   def handle_event(
         "find_optimal_recipes",
@@ -27,9 +21,6 @@ defmodule VesperiaWeb.Live.OptimalRecipes do
 
     {:ok, optimal_recipe_combos} = RecipeOptimizer.optimalize_recipes(store, visit)
 
-    {:noreply,
-     socket
-     |> assign(optimal_recipe_combos: optimal_recipe_combos)
-     |> update(:requests, &(&1 + 1))}
+    {:noreply, assign(socket, optimal_recipe_combos: optimal_recipe_combos)}
   end
 end
